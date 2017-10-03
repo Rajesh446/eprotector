c29f61ef95ced2f7bbeb8508444c904b34aefc63
Dhakchianandan <dhakchianandan@gmail.com>
2017-10-03 18:28:34 +0530

    Animation changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -73,19 +73,19 @@
   }
 
   function responsivefy(svg) {
-    var container = d3.select(svg.node().parentNode),
-      width = parseInt(svg.style('width')),
-      height = parseInt(svg.style('height')),
-      aspect = width / height;
-    svg.attr('viewBox', '0 0 ' + width + ' ' + height)
-      .attr('preserveAspectRatio', 'xMinYMid')
-      .call(resize);
-    d3.select(window).on('resize.' + container.attr('id'), resize);
-    function resize() {
-      var targetWidth = parseInt(container.style('width'));
-      svg.attr('width', targetWidth);
-      svg.attr('height', Math.round(targetWidth / aspect));
-    }
+    // var container = d3.select(svg.node().parentNode),
+    //   width = parseInt(svg.style('width')),
+    //   height = parseInt(svg.style('height')),
+    //   aspect = width / height;
+    // svg.attr('viewBox', '0 0 ' + width + ' ' + height)
+    //   .attr('preserveAspectRatio', 'xMinYMid')
+    //   .call(resize);
+    // d3.select(window).on('resize.' + container.attr('id'), resize);
+    // function resize() {
+    //   var targetWidth = parseInt(container.style('width'));
+    //   svg.attr('width', targetWidth);
+    //   svg.attr('height', Math.round(targetWidth / aspect));
+    // }
   }
 
   // Your custom JavaScript goes here
@@ -528,3 +528,44 @@
 
   createGaugeChart(window.d3, '.health-indicator');
 })();
+
+window.$(function() {
+  function slideBeforeScan() {
+    $('.a-slider').addClass('a-2');
+    $('.a-slider').removeClass('a-3');
+    $('.a-slider').removeClass('a-1');
+    setTimeout(function() {
+      doScan();
+    }, 1000);
+  }
+
+  function doScan() {
+    $('.a-scanner').addClass('scanner');
+    setTimeout(function() {
+      slideAfterScan();
+    }, 1500);
+  }
+
+  function slideAfterScan() {
+    $('.a-scanner').removeClass('scanner');
+    $('.a-slider').addClass('a-3');
+    $('.a-slider').removeClass('a-2');
+
+    setTimeout(function() {
+      cleanUp();
+    }, 1000);
+  }
+
+  function cleanUp() {
+    $('.a-slider').removeClass('a-3');
+    $('.a-slider').addClass('a-1');
+
+    setTimeout(function() {
+      slideBeforeScan();
+    }, 1000);
+  }
+
+  setTimeout(function() {
+    slideBeforeScan();
+  }, 1000);
+});
